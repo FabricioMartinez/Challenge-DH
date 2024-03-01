aed8b8aea070e762931c5a66db1fb1b981dfca3a
mariajazmin <lamasmariajazmin@gmail.com>
2024-03-01 15:01:34 -0300

    trayendo cambios

--- Challenge Digital House/Frontend/src/App.jsx
+++ Challenge Digital House/Frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import Search from './components/Search/Search'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
         <Search/>
+
+
+        <Routes>
+          
+        </Routes>
     </>
   )
 }
