86b271552f765694bedfa18a65bf25fe8ba254af
CristinaYucra <yucra.cris.23@gmail.com>
2024-03-01 17:57:41 -0300

    css

--- Challenge Digital House/Backend/src/app.js
+++ Challenge Digital House/Backend/src/app.js
@@ -5,8 +5,13 @@ const apiApplicants = require('./Routes/applicantsApiRoutes')
 const apiProfessions = require('./Routes/professionsApiRoutes')
 // URL encode
 app.use(express.urlencoded({ extended: false }));
+//
+// Aquí estoy disponiendo la posibilidad para utilizar el seteo en los formularios para el uso de los métodos put o delete
+//app.use(methodOverride('_method'));
 
-
+// URL encode - Para que nos pueda llegar la información desde el formulario al req.body
+//app.use(express.urlencoded({ extended: false }));
+//
 
 // Llamado a las rutas de APIs
 app.use('/api', apiApplicants,apiProfessions)
