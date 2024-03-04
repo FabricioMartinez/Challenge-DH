40eb4cb73a165cc416ddf44ebe1e808c840df352
heredianicolas <herediarobertonicolas@gmail.com>
2024-03-03 22:51:59 -0300

    agregando ruta de error

--- Challenge Digital House/Frontend/src/App.jsx
+++ Challenge Digital House/Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home/Home'
 import Applicants from './components/Applicants/ApplicantsSection'
 import Professions from './components/Professions/Professions'
 import Info from './components/ApplicantsInfo/Info'
+import NotFound from './components/NotFound/NotFound'
 import {Switch, Route} from 'react-router-dom'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path={'/applicants'} exact component={Applicants}></Route>
           <Route path={'/applicants/:id'} exact component={Info}></Route>
           <Route path={'/professions'} exact component={Professions}></Route>
+          <Route component={NotFound}></Route>
         </Switch>
     </>
   )
---
+++ Challenge Digital House/Frontend/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css'
+
+function NotFound (){
+    return(
+        <div className="container-404">
+            <h2>error 404 not found</h2>
+        </div>
+    )
+}
+
+export default NotFound;
